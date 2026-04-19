6c1d0ca45e827ed97fc57abca9157865ebad891c
nettoluis <luisnetto.2013@gmail.com>
2026-04-19 20:55:13 -0300

    adds: TwoSum O(n) solution
    hash table for storing the known values

--- TwoSum.java
+++ TwoSum.java
@@ -2,13 +2,17 @@ import java.util.*;
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> diffs = new HashMap<>();
+
         for (int i = 0; i < nums.length; i++) {
-           for (int j = i+1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[]{i, j};
-                } 
-           } 
+            int diff = target - nums[i];
+            if (diffs.containsKey(diff)) {
+                return new int[]{diffs.get(diff), i};
+            }
+
+            diffs.put(nums[i], i);
         }
-        return new int[]{-1};
+        return new int[]{-1,-1}
     }
 }
+
